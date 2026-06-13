b7d6d43e6ef11412afcd1d8943fee245b894f13a
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2026-06-12 22:03:48 -0500

    add @Id annotation to Product record for database mapping

--- src/main/java/com/challenge/products/domain/Product.java
+++ src/main/java/com/challenge/products/domain/Product.java
@@ -2,10 +2,12 @@ package com.challenge.products.domain;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("products")
 public record Product(
+    @Id
     Long id,
     String name,
     String description,
